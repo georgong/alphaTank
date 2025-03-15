a90a68cdb3d986e8255ca9fb08822fff3e8b1e30
Kevin Bian <kaiwenbian107@gmail.com>
2025-03-14 23:39:53 -0700

    added inference

--- inference.py
+++ inference.py
@@ -5,6 +5,7 @@ from env.gym_env import MultiAgentEnv
 from train_ppo_ppo import RunningMeanStd
 from train_ppo_bot import PPOAgent
 from env.bots.bot_factory import BotFactory
+import pygame
 
 def load_agents(env, device, mode='agent', bot_type='smart'):
     """Loads trained agents from the saved models."""
@@ -40,8 +41,13 @@ def run_inference(mode, bot_type='smart'):
     obs = torch.tensor(obs, dtype=torch.float32).to(device).reshape(env.num_tanks, -1)
     obs_dim = env.observation_space.shape[0] // len(agents)
     obs_norm = RunningMeanStd(shape=(env.num_tanks, obs_dim), device=device)
+    
+    agent_wins = 0
+    bot_wins = 0
 
     while True:
+        initial_tank0_alive = env.game_env.tanks[0].alive
+        initial_tank1_alive = env.game_env.tanks[1].alive
         with torch.no_grad():
             obs_norm.update(obs)
             obs = obs_norm.normalize(obs)
@@ -59,11 +65,28 @@ def run_inference(mode, bot_type='smart'):
         next_obs_np, _, done_np, _, _ = env.step(actions_list)
         obs = torch.tensor(next_obs_np, dtype=torch.float32).to(device).reshape(env.num_tanks, -1)
 
-        if np.any(done_np):
-            print("[INFO] Environment reset triggered.")
-            obs, _ = env.reset()
-            obs = torch.tensor(obs, dtype=torch.float32).to(device).reshape(env.num_tanks, -1)
-        
+        if mode == 'agent':
+            if np.any(done_np):
+                print("[INFO] Environment reset triggered.")
+                obs, _ = env.reset()
+                obs = torch.tensor(obs, dtype=torch.float32).to(device).reshape(env.num_tanks, -1)
+            
+        if mode == 'bot':
+            if np.any(done_np):
+                if initial_tank0_alive and not env.game_env.tanks[0].alive:
+                    agent_wins += 1
+                    print(f"Score - {bot_type}: {bot_wins}, Agent: {agent_wins}")
+                    
+                elif initial_tank1_alive and not env.game_env.tanks[1].alive:
+                    bot_wins += 1
+                    print(f"Score - {bot_type}: {bot_wins}, Agent: {agent_wins}")
+
+                else: # tie
+                    print(f"Score - {bot_type}: {bot_wins}, Agent: {agent_wins}: Ties")
+                
+                obs, _ = env.reset()
+                obs = torch.tensor(obs, dtype=torch.float32).to(device).reshape(env.num_tanks, -1)
+                
         env.render()
 
 if __name__ == "__main__":
